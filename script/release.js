f42de7c940da86f5cd470f33233284645f0f5699
Ryo_gk <ryo.gk55@gmail.com>
2022-12-06 03:17:35 +0900

    import assertion

--- script/release.js
+++ script/release.js
@@ -1,5 +1,5 @@
 import { execa } from 'execa'
-import pkg from '../package.json'
+import pkg from '../package.json' assert { type: "json" }
 
 const run = (bin, args, opts = {}) => execa(bin, args, { stdio: 'inherit', ...opts })
 
