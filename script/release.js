974269f9086e6701c45fc0811a2799e49ad0168b
Ryo_gk <62658104+ryo-gk@users.noreply.github.com>
2023-02-03 01:12:01 +0900

    chore: tweak script

--- script/release.js
+++ script/release.js
@@ -1,15 +1,16 @@
-import { execa } from 'execa'
-import { readFileSync } from 'fs'
+const execa = require('execa')
+const version = require('../package.json').version
 
 const run = (bin, args, opts = {}) => execa(bin, args, { stdio: 'inherit', ...opts })
 
-const pkg = JSON.parse(readFileSync('package.json', 'utf-8'))
-const version = pkg.version
+async function start() {
+  await run('pnpm', ['run', 'build'])
 
-await run('pnpm', ['run', 'build'])
+  await run('git', ['tag', '-a', `v${version}`, '-m', `v${version}`])
 
-await run('git', ['tag', '-a', `v${version}`, '-m', `v${version}`])
+  await run('git', ['push', 'origin', `tags/v${version}`])
 
-await run('git', ['push', 'origin', `tags/v${version}`])
+  await run('npm', ['publish', './'])
+}
 
-await run('npm', ['publish', './'])
+start()
